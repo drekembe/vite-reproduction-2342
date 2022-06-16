972b0b309f1d2b2c3eaa50f1bfb5c8ec533ce44e
Miran Lipovaca <m.lipovaca@cargox.io>
2022-06-16 11:32:07 +0200

    Update components/App.vue

--- components/src/App.vue
+++ components/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import OlehWorld from './components/OlehWorld/OlehWorld.vue'
+import HelloWorld from './components/HelloWorld/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <OlehWorld msg="foo" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="foo" />
+  </div>
 </template>
 
 <style>
